b9c2796580938b5dd86f9ba91bc3951b5d968b46
Alexandre Dehlavi <alexandredehlavi@hotmail.com>
2022-07-04 13:56:41 +0200

    Changed movedim function with transpose

--- core/solver.py
+++ core/solver.py
@@ -246,7 +246,7 @@ class Solver(nn.Module):
 
         if show:
             from matplotlib import pyplot as plt
-            plt.imshow(results.movedim(0, 2).numpy(), interpolation='nearest')
+            plt.imshow(results.transpose(0, 1).transpose(1, 2).numpy(), interpolation='nearest')
             plt.show()
 
     @torch.no_grad()
